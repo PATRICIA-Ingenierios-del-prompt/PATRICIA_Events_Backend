60ed4f97d51ed96580db2f67905655989d328492
marianamalagon11 <marianamalagon11@gmail.com>
2026-07-13 20:58:23 -0500

    feat: add things for album

--- src/main/java/ingprompt/patricia/events/application/port/out/EventPublisherOut.java
+++ src/main/java/ingprompt/patricia/events/application/port/out/EventPublisherOut.java
@@ -1,13 +1,16 @@
 package ingprompt.patricia.events.application.port.out;
 
+import ingprompt.patricia.events.domain.enums.Category;
+
 import java.util.Set;
 import java.util.UUID;
 
 public interface EventPublisherOut {
-    void publishEventCreated(UUID eventId, String name, UUID ownerId, boolean linkedToParche);
+    void publishEventCreated(UUID eventId, String name, UUID ownerId, boolean linkedToParche, Category category);
     void publishEventLinkedToParche(UUID eventId, String eventName, UUID parcheId, String parcheName, UUID userId, Set<UUID> memberIds);
     void publishEventDeleted(UUID eventId, UUID parcheId);
     void publishEventStarted(UUID eventId, Set<UUID> participants);
     void publishEventEnded(UUID eventId);
     void publishIncidentReported(UUID eventId, UUID reportId, UUID reporterId);
+    void publishParticipantJoined(UUID eventId, UUID userId, Category category);
 }
